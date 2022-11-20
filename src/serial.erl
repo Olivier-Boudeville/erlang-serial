4b2a284ab9167c06c1e65464cdb2f356258405c7
Olivier Boudeville <olivier.boudeville@esperide.com>
2022-11-20 19:24:55 +0100

    start/2 to set 'priv' freely. Code reformatted.

--- src/serial.erl
+++ src/serial.erl
@@ -28,96 +28,112 @@
 -module(serial).
 -author('jb@erix.ericsson.se').
 
--export([start/0, start/1, init/1, loop/2]).
+-export([start/0, start/1, start/2, init/1, loop/2]).
 
 -include("serial.hrl").
 
 priv_dir() ->
-    case code:priv_dir(serial) of
-        {error, bad_name} ->
-            "./priv";
-        D ->
-            D
-    end.
+	case code:priv_dir(serial) of
+		{error, bad_name} ->
+			"./priv";
+		D ->
+			D
+	end.
 
 start() ->
-    start([]).
+	start([]).
+
+
+% From an escript, we might inherit from an incorrect 'priv' directory (e.g. the
+% one of Oceanic), so we provide a way of setting it explicitly.
+%
+start(Options,SerialPrivDir) ->
+	Pid = spawn_link(serial, init, [SerialPrivDir,self()]),
+	process_options(Pid, Options),
+	Pid.
+
 
 start(Options) ->
-    Pid = spawn_link(serial, init, [self()]),
-    process_options(Pid, Options),
-    Pid.
+	start(Options,_SerialPrivDir=priv_dir()).
+
 
 process_options(_Pid, []) ->
-    done;
+	done;
 process_options(Pid, [Opt | Opts]) ->
-    Pid ! Opt,
-    process_options(Pid, Opts).
+	Pid ! Opt,
+	process_options(Pid, Opts).
+
+
+init(SerialPrivDir, Pid) ->
+	process_flag(trap_exit, true),
+	Port = open_port({spawn, SerialPrivDir ++ "/bin/serial -erlang"},
+					 [binary, {packet, 2}]),
+	loop(Pid, Port).
+
 
 init(Pid) ->
-    process_flag(trap_exit, true),
-    Port = open_port({spawn, priv_dir() ++ "/bin/serial -erlang"}, [binary, {packet, 2}]),
-    loop(Pid, Port).
+	init(_SerialPrivDir=priv_dir(), Pid).
+
 
 loop(Pid, Port) ->
-    receive
-        {Port, {data, Bytes}} ->
-            Pid ! {data, Bytes},
-            serial:loop(Pid, Port);
-        {send, Bytes} ->
-            send_serial(Port, [?SEND, Bytes]),
-            serial:loop(Pid, Port);
-        {connect} ->
-            send_serial(Port, [?CONNECT]),
-            serial:loop(Pid, Port);
-        {disconnect} ->
-            send_serial(Port, [?DISCONNECT]),
-            serial:loop(Pid, Port);
-        {open, TTY} ->
-            send_serial(Port, [?OPEN, TTY]),
-            serial:loop(Pid, Port);
-        {close} ->
-            send_serial(Port, [?CLOSE]),
-            serial:loop(Pid, Port);
-        {speed, NewInSpeed, NewOutSpeed} ->
-            send_serial(Port, [
-                ?SPEED,
-                integer_to_list(NewInSpeed),
-                " ",
-                integer_to_list(NewOutSpeed),
-                0
-            ]),
-            serial:loop(Pid, Port);
-        {speed, NewSpeed} ->
-            send_serial(Port, [
-                ?SPEED,
-                integer_to_list(NewSpeed),
-                " ",
-                integer_to_list(NewSpeed),
-                0
-            ]),
-            serial:loop(Pid, Port);
-        {parity_odd} ->
-            send_serial(Port, [?PARITY_ODD]),
-            serial:loop(Pid, Port);
-        {parity_even} ->
-            send_serial(Port, [?PARITY_EVEN]),
-            serial:loop(Pid, Port);
-        {break} ->
-            send_serial(Port, [?BREAK]),
-            serial:loop(Pid, Port);
-        stop ->
-            stopped;
-        {'EXIT', Port, Why} ->
-            io:format("Port exited with reason ~w~n", [Why]),
-            exit(Why);
-        {'EXIT', Linked, Why} ->
-            io:format("Linked ~w exited with reason ~w~n", [Linked, Why]),
-            exit(Why);
-        OtherError ->
-            io:format("Received unknown message ~w~n", [OtherError]),
-            serial:loop(Pid, Port)
-    end.
+	receive
+		{Port, {data, Bytes}} ->
+			Pid ! {data, Bytes},
+			serial:loop(Pid, Port);
+		{send, Bytes} ->
+			send_serial(Port, [?SEND, Bytes]),
+			serial:loop(Pid, Port);
+		{connect} ->
+			send_serial(Port, [?CONNECT]),
+			serial:loop(Pid, Port);
+		{disconnect} ->
+			send_serial(Port, [?DISCONNECT]),
+			serial:loop(Pid, Port);
+		{open, TTY} ->
+			send_serial(Port, [?OPEN, TTY]),
+			serial:loop(Pid, Port);
+		{close} ->
+			send_serial(Port, [?CLOSE]),
+			serial:loop(Pid, Port);
+		{speed, NewInSpeed, NewOutSpeed} ->
+			send_serial(Port, [
+				?SPEED,
+				integer_to_list(NewInSpeed),
+				" ",
+				integer_to_list(NewOutSpeed),
+				0
+			]),
+			serial:loop(Pid, Port);
+		{speed, NewSpeed} ->
+			send_serial(Port, [
+				?SPEED,
+				integer_to_list(NewSpeed),
+				" ",
+				integer_to_list(NewSpeed),
+				0
+			]),
+			serial:loop(Pid, Port);
+		{parity_odd} ->
+			send_serial(Port, [?PARITY_ODD]),
+			serial:loop(Pid, Port);
+		{parity_even} ->
+			send_serial(Port, [?PARITY_EVEN]),
+			serial:loop(Pid, Port);
+		{break} ->
+			send_serial(Port, [?BREAK]),
+			serial:loop(Pid, Port);
+		stop ->
+			stopped;
+		{'EXIT', Port, Why} ->
+			io:format("Port exited with reason ~w~n", [Why]),
+			exit(Why);
+		{'EXIT', Linked, Why} ->
+			io:format("Linked ~w exited with reason ~w~n", [Linked, Why]),
+			exit(Why);
+		OtherError ->
+			io:format("Received unknown message ~w~n", [OtherError]),
+			serial:loop(Pid, Port)
+	end.
 
 send_serial(Port, Message) ->
-    Port ! {self(), {command, Message}}.
+	Port ! {self(), {command, Message}}.
