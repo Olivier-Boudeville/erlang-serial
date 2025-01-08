7b7d5b05a161f41e38119decb230e1b859cbf90c
Olivier Boudeville <olivier.boudeville@esperide.com>
2025-01-08 13:21:48 +0100

    Minor restyling.

--- examples/basic_example.erl
+++ examples/basic_example.erl
@@ -2,27 +2,31 @@
 
 -export([open/0, close/1, send/1, listen/0]).
 
+-define(target_tty, "/dev/ttyUSB0").
+
 open() ->
-  SerialPort = serial:start([{open, "/dev/ttyUSB0"}, {speed, 9600}]),
-  {ok, SerialPort}.
+	SerialPort = serial:start([{open, ?target_tty}, {speed, 9600}]),
+	{ok, SerialPort}.
 
 close(SerialPort) ->
-  SerialPort ! {close},
-  ok.
+	SerialPort ! {close},
+	ok.
 
 send(SerialPort) ->
-  SerialPort ! {send, "Hello World\r\n"},
-  ok.
+	SerialPort ! {send, "Hello World\r\n"},
+	ok.
 
 listen() ->
-  receive
-    % Receive data from the serial port on the caller's PID.
-    {data, Bytes} ->
-      io:format("~s", [Bytes]),
-      listen()
-  after
-    % Stop listening after 5 seconds of inactivity.
-    5000 ->
-      io:format("~n"),
-      ok
+	receive
+
+		% Receive data from the serial port on the caller's PID.
+		{data, Bytes} ->
+			io:format("~s", [Bytes]),
+			listen()
+
+	% Stop listening after 5 seconds of inactivity.
+	after 5000 ->
+		io:format("~n"),
+		ok
+
   end.
